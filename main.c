4976e38ea4f3cfdbc531e0e3f87a16839c3a1f32
Jordi <jordi.guetomatavera@alum.uca.es>
2018-03-22 20:41:52 +0100

    Subida 1 tras entrega en el Campus Virtual

--- main.c
+++ main.c
@@ -17,8 +17,9 @@
  */
 int main() {
     
-    char login[6], pass[9], perfil[15];
-    int elementos, exisUser=0, passOK=0, errorPass=0, i, id;
+    Usuarios L_Usuario[1];
+    char user[6], login[9], perfil[15];
+    int elementos, exisUser=0, passOK=0, i, id;
 
     printf("                                                                                             BHU##mgmgm#U\n"
            "                                                                                     BHUUmmgm            mm$\n"
@@ -32,24 +33,24 @@ int main() {
            "                    BHU##mgmm                                                                                     m\n"
            "                 Umm                                                                                               d\n"
            "              @$m                                                                 $                                #\n"
-           "             Am                                                               Y+===+!                @Y$           m\n"
-           "            q                                                               B(:|=+=+=     UUH    }+=U=;3            $\n"
-           "          m                                               VS               (=+=wgga;  Y''+==:+9  (++===]            #\n"
-           "          m                                               iii              E=++m    gm ===|=<+=  c++<wym            m\n"
-           "         D                                         VYYY$  zll    8Y1II9    [=|]        sw   c==$ k=+3               #\n"
-           "        @m                                     VYliillii  klid  liiilIi3   (+=j          DY!=+=3 k=+j               #\n"
-           "        X                    #1nd              clllvslli3 mil3  |vwmglIv   (++j         !;;:|=+3 E=+]               d\n"
-           "       @m              BUU$   nod      UH      kll3  piI3  ilv  w  @TlIl   c==]        (==umE=+]  =+)               m\n"
-           "       h           8VSvnnnnX  ono   XnnnnvX    EiIv  kiim  cIl   D1iivli$  k=|+      V :|=  #=|)  =+=              @m\n"
-           "       $          mvnnnnnnon  2on  hnnoooon$    iIvVYiI3V  Cvi$ Di%vgpil3  m;+=]    F: =|=9H(++=  >==              E\n"
-           "       m           nonm moon  Enn$ mom  mnnd    cvlilllliI kll3 CIl   iI3   c=|=)YY(:=$c=+=;<>==  aww              m\n"
-           "      #            2oo#  knn  mno$    8Snoo3    kilvwyg%ll3mlI3 zIiUB1lli    c=++==+=sm a=<a gm                     Bm\n"
-           "      D            Enod Uvn#   vo3  B1nvqooo    kll3   klI3 %II kiviivzva     w>|=<sy                             m\n"
-           "      E            mnoonnnonn$ ooX  1on  kno     ill   CIlj zll  oliam                                           Dm\n"
-           "      #             vooowwooo3 2on #nod  Snn$    %viUV1lIim kiim                                                @m\n"
-           "      E             ooo    non hnnm nooVSvon3    eIllilllm   m                                                  m\n"
-           "      m             kond   nonmmnod p1onnmwm     klllvay                                                       q\n"
-           "       $            hnod BSnon  nn3  mwm         muym                                                        Bq\n"
+           "             Am                                                               YBBBBB!                @Y$           m\n"
+           "            q                                                               BBB|BBBBB     UUH    BBBUBBB            $\n"
+           "          m                                               VS               BBBBwggaB  Y''BBBBB9  çBBBBBB            #\n"
+           "          m                                               iii              EBBBm    gm BBB|B<BB  cBB<wym            m\n"
+           "         D                                         VYYY$  zll    8Y1BB9    BB|B        sw   cBB$ kBBB               #\n"
+           "        @m                                     VYliillii  klid  liiilBiB   BBBj          DY!BBBB kBBj               #\n"
+           "        X                    #1nd              clllBslliB milB  |BwmglBB   BBBj         !BBB|BBB EBBB               d\n"
+           "       @m              BUU$   nod      UH      kllB  piBB  ilB  w  @TlBl   cBBB        BBBumEBBB  BBB               m\n"
+           "       h           8VSBnnnnX  ono   XnnnnBX    EiBB  kiim  cBl   D1iiBli$  kB|B      V B|B  #B|B  BBB              @m\n"
+           "       $          mBnnnnnnon  2on  hnnoooon$    iBBVYiBBV  CBi$ DiBBgpilB  m;BBB    FB B|B9HBBBB  >BB              E\n"
+           "       m           nonm moon  Enn$ mom  mnnd    cBlilllliB kllB CBl   iBB   cB|BBYYBBB$cBBB;<>BB  aww              m\n"
+           "      #            2oo#  knn  mno$    8SnooB    kilBwygBllBmlBB zBiUB1lli    cBBBBBBBsm aB<a gm                     Bm\n"
+           "      D            Enod UBn#   BoB  B1nBqooo    kllB   klBB BBB kiBiiBzBa     w>|B<sy                             m\n"
+           "      E            mnoonnnonn$ ooX  1on  kno     ill   CBlj zll  oliam                                           Dm\n"
+           "      #             BooowwoooB 2on #nod  Snn$    BBiUV1lBim kiim                                                @m\n"
+           "      E             ooo    non hnnm nooVSBonB    eBllilllm   m                                                  m\n"
+           "      m             kond   nonmmnod p1onnmwm     klllBay                                                       q\n"
+           "       $            hnod BSnon  nnB  mwm         muym                                                        Bq\n"
            "       #            moooonnnn   wmm                                                                        @G\n"
            "       Z             noonnog                                                                             8Gm\n"
            "        $            owmm                                                                          8HH#mm\n"
@@ -67,48 +68,121 @@ int main() {
     
     printf("\n\nINTRODUZCA SU USUARIO Y CONTRASEÑA PARA ACCEDER:");
     
-    do{
-        printf("Introduce nombre de usuario:\n");
-        fgets (login, 6, stdin);
+    printf("Introduce nombre de usuario:\n");                                   //Solicitamos el nombre del usuario
+    fgets (user, 6, stdin);
 
-        //Conmrpobamos con el array de usuarios la existencia del usuario, en caso contrario error
-        i=0;
-        while(i<elementos || exisUser==0){
-            if(strcmp(login,L_Usuario[i].login)){
-                exisUser=1;
-                strcpy(L_Usuario[i].perfil,perfil);
-            }
+    i=0;
+    while(i<elementos || exisUser==0){                                          //Buscamos el nombre del usuario si existe
+        if(strcmp(user,L_Usuario[i].User)){
+            exisUser=1;                                                         //Si hay una concidencia guardamos y salimos
+            strcpy(L_Usuario[i].Perfil_usuario,perfil);
         }
-
-        if(exisUser==0){
-            printf("\nError el alumno no indicado no existe\n");
-        }
-    }while(exisUser==0);
+        i++;
+    }
     
-    do{
-        printf("Introduce el password:\n");
-        fgets (pass, 9, stdin);
-        
-        //Conmrpobamos con el array de usuarios si la contraseña, en caso contrario error
-        i=0;
-        while(i<elementos || passOK==0){
-            if(strcmp(pass,L_Usuario[i].pass)){
-                passOK=1;
-                id=L_Usuario[i].id_usuario  ;
+    if(exisUser==0){                                                            //Si el usuario no existe creamos una cuenta nueva
+        printf("\nUsuario indicado no existe, accediendo para crear una cuenta\n");
+        altaUsuarioInicio(L_Usuario,user);
+    }
+    else{
+        i--;
+       
+        printf("Introduce el password:\n");                                     //Solicitamos la contraseña del usuario
+        fgets (login, 9, stdin);
+
+        if(strcmp(login,L_Usuario[i].Login)){
+            passOK=1;
+            id=L_Usuario[i].Id_usuario;
+            if(passOK==1 && exisUser==1){
+                menu_principal(strcmp(perfil,"administrador"),id);
             }
         }
-
-        if(passOK==0){
+        else{
             printf("\nError contraseña erronea\n");
-            errorPass++;
-            printf("Te quedan %d intentos.\n"3-errorPass);
+            printf("Saliendo del sistema\n");
         }
-    }while(passOK==0 && errorPass<3);
-    
-    if(passOK==1 && exisUser==1){
-        menu_principal(strcmp(perfil,"administrador"),id);
     }
 
     return (0);
 }
 
+// Cabecera: void menu_principal(E entero opc, E entero id)
+// Precondicion: Valor 1 para administrados, valor <> 1 para usuario
+// Poscondicion:  Accede a los distintos menus de la aplicación, no retorna nada
+void menu_principal(int opc, int id)
+{
+    int x;
+    
+    if(opc=1){
+        do
+        {
+            printf("Introduzca la opcion que desea\n\n "
+                    "1: Menú usuarios\n "
+                    "2: Menú vehiculos\n "
+                    "3: Menú viajes\n "
+                    "4: Menú incidencias\n "
+                    "0: salir\n");
+            scanf("%d",&x);
+            switch (x)
+            {
+            case 0:
+                exit(0);
+            case 1:
+                menu_usuario();
+                menu_principal(opc,id);
+                break;
+            case 2:
+                menu_alumno();
+                menu_principal(opc,id);
+                break;
+            case 3:
+                menuMaterias();
+                menu_principal(opc,id);
+                break;
+            case 4:
+                menuHorarios();
+                menu_principal(opc,id);
+                break;
+            default:
+                printf("Error al elegir la opcion.\t");
+                break;
+            }
+        }while(x!=0);
+    }
+    else{
+        do
+        {
+            printf("Introduzca la opcion que desea\n\n "
+                    "1: Perfil\n "
+                    "2: Menú vehiculos\n "
+                    "3: Menú viajes\n "
+                    "4: Menú incidencias\n "
+                    "0: salir\n");
+            scanf("%d",&x);
+            switch (x)
+            {
+            case 0:
+                exit(0);
+            case 1:
+                menu_usuario();
+                menu_principal(opc,id);
+                break;
+            case 2:
+                menu_alumno();
+                menu_principal(opc,id);
+                break;
+            case 3:
+                menuMaterias();
+                menu_principal(opc,id);
+                break;
+            case 4:
+                menuHorarios();
+                menu_principal(opc,id);
+                break;
+            default:
+                printf("Error al elegir la opcion.\t");
+                break;
+            }
+        }while(x!=0);
+    }
+}
