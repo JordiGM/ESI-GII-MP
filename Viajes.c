7967623db64e653bf5143038cf2569873a5b4a4e
ruben <ruben@ruben-HP-Pavilion-g6-Notebook-PC>
2018-04-02 11:22:33 +0200

    Viajes.h "completo"

--- Viajes.c
+++ Viajes.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "Viajes.h"
-#include "fichero.h"
+#include "ficheros.h"
 
 /*Cabecera: void publicar_viaje(Viaje*)
 Precondición: Recibe cadenas de carácteres de un viaje.
@@ -11,36 +12,36 @@ void publicar_viaje(){
     Viaje v;
     char c;
 
-	if(
+	if(c);/*La c no va ahí, es una medida provisional para que no de un error momentaneo.*/
 		do{
 			printf("Introduzca el id del viaje.");
-			gets(v.Id_viaje);
+			scanf("%s", v.Id_viaje);
         
 			printf("Introduzca la matricula del vehiculo con el que va a realizar el viaje.");
-			gets(v.Id_mat);
+			scanf("%s", v.Id_mat);
         
 			printf("Introduzca la fecha en la que se va a realizar el viaje.");
-			gets(v.F_inic);
+			scanf("%s", v.F_inic);
         
 			printf("Introduzca la hora de inicio del viaje.");
-			gets(v.H_inic);
+			scanf("%s", v.H_inic);
         
 			printf("Introduzca la hora de finalización del viaje.");
-			gets(v.H_fin);
+			scanf("%s", v.H_fin);
         
 			printf("Introduzca el numero de plazas libres que hay en el viaje.");
-			gets(v.Plazas_libre);
+			scanf("%s", v.Plazas_libre);
         
 			printf("Introduzca el sentido del viaje (ida o vuelta.");
-			gets(v.Sentido);
+			scanf("%s", v.Sentido);
         
 			printf("Introduzca el importe del viaje.");
-			gets(v.Importe);
+			scanf("%s", v.Importe);
         
 			printf("Introduzca el estado del viaje (abierto, cerrado,iniciado, finalizado o anulado).");
-			gets(v.Estado);
+			scanf("%s", v.Estado);
 
-			printf("%c\n%c\n%c\n%c\n%c\n%c\n%c\n%c\n%c\n", v.Id_viaje, v.Id_mat, v.F_inic, v.H_inic, v.H_fin, v.Plazas_libre, v.Sentido, v.Importe, v.Estado);
+			printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n", v.Id_viaje, v.Id_mat, v.F_inic, v.H_inic, v.H_fin, v.Plazas_libre, v.Sentido, v.Importe, v.Estado);
 			printf("¿Son correctos los datos? s/n\n");
 			scanf("%c", &c);
 		} while(c!='s');
@@ -52,7 +53,8 @@ Postcondición: Permite al usuario eliminar un viaje previamente creado por él 
 
 void eliminar_viaje(Viaje *viaje, int n){
 
-    int id, i;
+    int i;
+    char id;
     listar_viaje(&viaje, n);
     printf("Introduce el id del viaje a eliminar");
     scanf("%i", id);
