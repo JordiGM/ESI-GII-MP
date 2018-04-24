201878a90c5d6c9e760b38d23749d7bcf154a8a1
gemac <gemac@DESKTOP-OKN6RA4>
2018-04-24 20:27:03 +0200

    Falta poner variables al menu y revisar problemas de nombreas

--- Vehiculo.h
+++ Vehiculo.h
@@ -1,10 +1,3 @@
-/* 
- * File:   Vehiculo.h
- * Author: gemac
- *
- * Created on 21 de marzo de 2018, 17:05
- */
-
 #ifndef VEHICULO_H
 #define VEHICULO_H
 
@@ -13,5 +6,7 @@ void menu_vehiculo();
 void alta_vehiculo();
 void baja_vehiculo();
 void modificar_vehiculo();
+
+
 #endif
 
