2593beb83a7d2777d62bd0583bfecdf30840916f
Jordi <jordi.guetomatavera@alum.uca.es>
2018-05-08 11:11:05 +0200

    Finalizada la función de eleminar un paso

--- Pasos.c
+++ Pasos.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include "ficheros.c"
 #include "comun.h"
+#include "Pasos.h"
 
 void publicar_paso(Pasos *lista, int* numPasos, char* idViaje){
     char *poblacion;
@@ -10,7 +11,8 @@ void publicar_paso(Pasos *lista, int* numPasos, char* idViaje){
         
     poblacion = leer_campo(TAM_POB_PAS,"Población por la que pasa el viaje");
     
-    lista = (Pasos *) realloc(lista, (*numPasos + 1) * sizeof (Pasos)); //Reserva memoria para los elementos restantes
+    if (! *numPasos) lista = (Pasos *) malloc((*numPasos + 1) * sizeof (Pasos)); //Reserva memoria para el primer elemento
+    else lista = (Pasos *) realloc(lista, (*numPasos + 1) * sizeof (Pasos)); //Reserva memoria para los elementos restantes
 
     lista[*numPasos].Id_viaje = idViaje; //Asiganamos los valores recogidos al elemento
     lista[*numPasos].Poblacion = poblacion;
@@ -22,10 +24,18 @@ void eliminar_paso(Pasos *lista, int* numPasos, char* idViaje, char* poblacion){
     
     int indice = 0;
     
-    while(indice < *numPasos || (!strcmp(idViaje,lista[indice].Id_viaje) && !strcmp(poblacion, lista[indice].Poblacion)))
-        indice++;
-    
-    lista[indice].Eliminado = "Si";
+    if(*numPasos == 1){
+        free(lista);
+    }
+    else{
+        while(indice < *numPasos || (!strcmp(idViaje,lista[indice].Id_viaje) && !strcmp(poblacion, lista[indice].Poblacion)))
+            indice++;
+        
+        if(*numPasos-1 != indice){
+            lista[indice] = lista[*numPasos-1];
+        }
+        lista = (Pasos *) realloc(lista, (*numPasos - 1) * sizeof (Pasos));
+    }  
 }
 
 void listar_paso(Pasos *lista, int* numPasos, char* idViaje){
